c934cf988b2d5ed2297dbad1f86014044417b7e0
Henrique Santana <hsatana.sousass@gmail.com>
2023-02-11 03:38:30 -0300

    feat: método create user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,7 @@
 
 module Api
   class UsersController < ApplicationController
+    before_action :authenticate_api_user!
     before_action :set_user, only: %i[show destroy]
 
     def index
@@ -13,11 +14,25 @@ module Api
       render(json: @user)
     end
 
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        render(json: @user)
+      else
+        render(json: @user, status: :unprocessable_entity)
+      end
+    end
+
     def destroy
       @user.destroy
     end
 
     private
+
+    def user_params
+      params.require(:user).permit(:email, :name, :last_name, :gender, :height, :weight, :role,
+                                   :date_of_birth, :objective, :password, :password_confirmation)
+    end
 
     def set_user
       @user = User.find(params[:id])
